d37c6a62543d87f9c19e4b359573bca2d8b15b34
Alexander Kacharava <alexander@muze.nl>
2023-02-10 13:20:12 +0100

    add solid getProfile and saveProfile actions

--- www/api/data/base-components/solid-auth-fetcher/actions/getProfile.js
+++ www/api/data/base-components/solid-auth-fetcher/actions/getProfile.js
@@ -1,15 +1,30 @@
 function() {
-  var session = solidAuthFetcher.getSession()
+  return solidAuthFetcher.getSession()
   .then(function(session) {
     if (session) {
       editor.pageData.webId = session.webId;
-      solidAuthFetcher.fetch(session.webId)
-      .then(function(response) {
-        return response.text();
-      })
-      .then(function(profile) {
-        console.log(profile);
-      });
+      if (!editor.pageData.profileLoaded) {
+        console.log("fetching profile");
+        return solidAuthFetcher.fetch(session.webId)
+        .then(function(response) {
+          return response.text();
+        })
+        .then(function(profile) {
+          console.log(profile);
+          editor.pageData.profileLoaded = true;
+          // FIXME: actually fetch fields from the profile
+          return {
+            "woonplaats" : "Utrecht",
+            "aow-leeftijd-behaald" : false,
+            "ouder-dan-21" : true,
+            "alleenstaande" : true,
+            "thuiswonende-kinderen" : true
+          }
+        });
+      } else {
+        console.log("returning cached profile");
+        return editor.pageData.profile;
+      }
     }
   }); 
 }
---
+++ www/api/data/base-components/solid-auth-fetcher/actions/saveProfile.js
@@ -0,0 +1,6 @@
+function() {
+  // FIXME: store the editor.pageData.profile information in Solid;
+  return new Promise(function(resolve, reject) {
+	resolve(true);    
+  });
+}
