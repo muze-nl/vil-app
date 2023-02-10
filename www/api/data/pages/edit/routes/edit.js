df5eabe64cb449173821d120ea1e0439ebc80156
Alexander Kacharava <alexander@muze.nl>
2023-02-10 13:22:50 +0100

    add the edit page

--- www/api/data/pages/edit/routes/edit.js
+++ www/api/data/pages/edit/routes/edit.js
@@ -1,3 +1,7 @@
 function(params) {
-  editor.pageData.page = "edit";
+  simplyApp.actions.getProfile()
+  .then(function(profile) {
+    editor.pageData.profile = profile;
+    editor.pageData.page = "edit";
+  });
 }
