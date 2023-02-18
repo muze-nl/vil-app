cba0fe982a4626f9729b015620b6a53e50705b6c
Yvo Brevoort <yvo@muze.nl>
2023-02-18 11:08:17 +0100

    timeout for page complete to let the transformer go first

--- www/api/data/pages/view/routes/view.js
+++ www/api/data/pages/view/routes/view.js
@@ -1,9 +1,10 @@
 function(params) {
   simplyApp.actions.getProfile()
   .then(function(profile) {
-    console.log(profile);
     editor.pageData.profile = profile;
-    editor.pageData.profileFieldsComplete = simplyApp.actions.isProfileComplete(profile);
     editor.pageData.page = "view";
+    setTimeout(function() {
+      editor.pageData.profileFieldsComplete = simplyApp.actions.isProfileComplete(editor.pageData.profile);
+    });
   });
 }
