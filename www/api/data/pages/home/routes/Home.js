030084a25d16e105791109f8e0b806bf8f635a7c
Yvo Brevoort <yvo@muze.nl>
2023-02-18 11:09:30 +0100

    add (removed) handler for solid-authn

--- www/api/data/pages/home/routes/Home.js
+++ www/api/data/pages/home/routes/Home.js
@@ -1,3 +1,10 @@
 function() {
-  editor.pageData.page = "home";
+  if (false && document.location.search.match("code")) { // removed, needed for solid-client-authn-browser
+    solidClientAuthentication.default.handleIncomingRedirect({ restorePreviousSession : true })
+    .then(function() {
+      document.location.hash = "#view";
+    });
+  } else {
+    editor.pageData.page = "home";
+  }
 }
